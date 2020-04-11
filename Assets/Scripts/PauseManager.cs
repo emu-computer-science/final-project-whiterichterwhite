de733df401072048e9c800179daabdde6e8d6c2d
NeroCarson <55407930+NeroCarson@users.noreply.github.com>
2020-04-11 17:29:26 -0400

    added a puase menu

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pausePanel;
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+           ;        
+        }
+    }
+
+    public void Resume() {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Pause() {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    //method to be used when we have our main menu scene actually made
+    //public void mainMenu() {
+    //    //whatever our actual main menu scene is going to be
+    //    SceneManager.LoadScene();
+    //         Time.timeScale = 1f;
+    //}
+
+}
