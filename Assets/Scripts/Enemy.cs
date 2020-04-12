f4e2002c3262c3aef2744639c3ce9f237755676f
NeroCarson <55407930+NeroCarson@users.noreply.github.com>
2020-04-12 15:13:01 -0400

    Major changes done.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum EnemyState{
+    idle,
+    walk,
+    attack,
+    stagger
+}
+
+
+public class Enemy : MonoBehaviour
+{
+    public EnemyState currentState;
+    public FloatValue maxHealth;
+    public float health;
+    public string enemyName;
+    public int baseAttack;
+    public float moveSpeed;
+
+    public void Knock(Rigidbody2D myRigidbody, float knockTime)
+    {
+        StartCoroutine(KnockCo(myRigidbody, knockTime));
+    }
+
+    private IEnumerator KnockCo(Rigidbody2D myRigidBody, float knockTime)
+    {
+        if (myRigidBody != null)
+        {
+            yield return new WaitForSeconds(knockTime);
+            myRigidBody.velocity = Vector2.zero;
+            myRigidBody.GetComponent<Enemy>().currentState = EnemyState.idle;
+            myRigidBody.velocity = Vector2.zero;
+        }
+    }
+}
