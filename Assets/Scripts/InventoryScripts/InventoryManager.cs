507a5fb2a34de93d87a09e5c4ea9a57092f91d7c
NeroCarson <55407930+NeroCarson@users.noreply.github.com>
2020-04-12 15:11:00 -0400

    Built Inventory system, unable to use items yet

---
+++ Assets/Scripts/InventoryScripts/InventoryManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InventoryManager : MonoBehaviour
+{
+
+    [Header("Inventory Info")]
+    public PlayerInventory playerInv;
+    [SerializeField] private GameObject blankSlot;
+    [SerializeField] private GameObject invPanel;
+    [SerializeField] private TextMeshProUGUI descText;
+    [SerializeField] private GameObject useButton;
+
+    public void SetTandB(string desc, bool buttonActive) {
+
+        descText.text = desc;
+
+        if (buttonActive) {
+            useButton.SetActive(true);
+        }
+        else{
+            useButton.SetActive(false);
+        }
+    }
+
+
+    public void MakeInventorySlots() {
+
+        if (playerInv) {
+            for (int i = 0 ; i < playerInv.playerInv.Count; i++) {
+                GameObject temp = Instantiate(blankSlot,invPanel.transform.position,Quaternion.identity);
+                temp.transform.SetParent(invPanel.transform);
+                InventorySlot newSlot = temp.GetComponent<InventorySlot>();
+
+                
+                newSlot.Setup(playerInv.playerInv[i], this);
+
+
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MakeInventorySlots();
+        SetTandB("", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/InventoryScripts/InventorySlot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    [Header("Things to change in the UI")]
+    [SerializeField] private TextMeshProUGUI itemNum;
+    [SerializeField] private Image itemImage;
+
+    [Header("Variables from the item")]
+    public InventoryItem thisItem;
+    public InventoryManager thisManager;
+
+    public void Setup(InventoryItem item, InventoryManager manager) {
+
+        thisItem = item;
+        thisManager = manager;
+
+        if (thisItem) {
+            itemImage.sprite = thisItem.itemImage;
+            itemNum.text = "" + thisItem.numberHeld;
+        }
+
+    }
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
