028b495539f025286358033e0205353fcf881cb7
NeroCarson <55407930+NeroCarson@users.noreply.github.com>
2020-04-19 16:17:54 -0400

    Minor Inventory bug fixed

---
+++ Assets/Scripts/InventoryScripts/HealthReaction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthReaction : MonoBehaviour
+{
+    public float playerHealth;
+   
+
+    public void Use() { 
+        
+    }
+
+}
