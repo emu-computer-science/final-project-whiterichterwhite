2dae0400f293be4f283c23e022ae76e8e7464efb
NeroCarson <55407930+NeroCarson@users.noreply.github.com>
2020-04-19 21:14:30 -0400

    Just modified first scene added a door,

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public string sceneToLoad;
+    public Vector2 playerPos;
+    public PlayerVector playerStored;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger){
+            playerStored.startVal = playerPos;
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/PlayerVector.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class PlayerVector : ScriptableObject
+{
+    public Vector2 startVal;
+}
